8051b2f4517a77a4a4a7c7e4faca4aa40dc7a556
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-12-26 18:54:14 +0200

    Add Common Project and create GlobalConstants class

---
+++ LawyerServices.Common/GlobalConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace LawyerServices.Common
+{
+    public static class GlobalConstants
+    {
+
+        public const string AdministratorRoleName = "Administrator";
+
+        public const string AdministratorUserName = "neshev1978@gmail.com";
+
+        public const string AdministratorPassword = "Administrator";
+
+        public const string AdministratorEmail = "neshev1978@gmail.com";
+
+    }
+}
