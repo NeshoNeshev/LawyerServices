2569bd4163f844aa2467844e6a49682fb3b363a4
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-12-26 19:23:24 +0200

    Add database

---
+++ Data/LawyerServices.Data/DesignTimeDbFactory.cs
@@ -0,0 +1,37 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace LawyerServices.Data
+{
+    public class DesignTimeDbFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+
+            IConfiguration configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json").Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString =
+                configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options, new OperationalStoreOptionsMigrations());
+        }
+    }
+    public class OperationalStoreOptionsMigrations : IOptions<OperationalStoreOptions>
+    {
+        public OperationalStoreOptions Value => new OperationalStoreOptions()
+        {
+            DeviceFlowCodes = new TableConfiguration("DeviceCodes"),
+            EnableTokenCleanup = false,
+            PersistedGrants = new TableConfiguration("PersistedGrants"),
+            TokenCleanupBatchSize = 100,
+            TokenCleanupInterval = 3600,
+        };
+    }
+}
