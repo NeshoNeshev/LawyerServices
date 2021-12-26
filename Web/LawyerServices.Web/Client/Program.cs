e7d0f1722c7144e137007dcf3adc1eccf8c6c24b
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-12-26 19:00:08 +0200

    Add Radzen to Blazor Client Side

--- Web/LawyerServices.Web/Client/Program.cs
+++ Web/LawyerServices.Web/Client/Program.cs
@@ -2,6 +2,7 @@ using LawyerServices.Web.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,9 +11,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddHttpClient("LawyerServices.Web.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
+
+builder.Services.AddHttpClient("LawyerServices.Web.ServerAPI.NoAuthenticationClient",
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("LawyerServices.Web.ServerAPI"));
 
+builder.Services.AddScoped(typeof(AccountClaimsPrincipalFactory<RemoteUserAccount>), typeof(RolesAccountClaimsPrincipalFactory));
+
 builder.Services.AddApiAuthorization();
+
+//Radzen
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 await builder.Build().RunAsync();
