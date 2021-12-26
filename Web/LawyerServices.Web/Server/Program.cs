f3e533b878b985db5f6707cebc6e89092598df36
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-12-26 19:17:40 +0200

    Add Roles

--- Web/LawyerServices.Web/Server/Program.cs
+++ Web/LawyerServices.Web/Server/Program.cs
@@ -1,8 +1,16 @@
-using LawyerServices.Web.Server.Data;
-using LawyerServices.Web.Server.Models;
+using IdentityModel;
+using LawyerServices.Data;
+using LawyerServices.Data.Models;
+using LawyerServices.Data.Repositories;
+using LawyerServices.Data.Seeding;
+using LawyerServices.Services.Mapping;
+using LawyerServices.Web.Shared;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Radzen;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,19 +18,46 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions).AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>(options =>
+                {
+                    const string OpenId = "openid";
+
+                    options.IdentityResources[OpenId].UserClaims.Add(JwtClaimTypes.Email);
+                    options.ApiResources.Single().UserClaims.Add(JwtClaimTypes.Email);
+
+                    options.IdentityResources[OpenId].UserClaims.Add(JwtClaimTypes.Role);
+                    options.ApiResources.Single().UserClaims.Add(JwtClaimTypes.Role);
+                });
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove(JwtClaimTypes.Role);
 
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+//Application services
+
+
+// Data repositories
+builder.Services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddScoped<IDbQueryRunner, DbQueryRunner>();
+
+
+//Radzen
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 var app = builder.Build();
 
@@ -38,7 +73,18 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+using (var serviceScope = app.Services.CreateScope())
+{
+    AutoMapperConfiguration.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+    IServiceProvider serviceProvider = serviceScope.ServiceProvider;
+    var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    ApplicationDbInitialiser.SeedRoles(roleManager);
+    ApplicationDbInitialiser.SeedUsers(userManager);
 
+}
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
