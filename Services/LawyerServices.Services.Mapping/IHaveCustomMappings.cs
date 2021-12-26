c1d50189f46cde3f753d477d6bd87802fc4b7d7a
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-12-26 18:52:00 +0200

    Implement AutoMapper

---
+++ Services/LawyerServices.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace LawyerServices.Services.Mapping
+{
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configuration);
+    }
+}
