d40739966532b1e838b0c67da8575bdc967c54b9
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2021-12-26 18:22:25 +0200

    DbQueryRunner

---
+++ Data/LawyerServices.Data/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace LawyerServices.Data.Repositories
+{
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+         where TEntity : class, IDeletable
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
