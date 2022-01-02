aabf6ed7b08b78332a592d59a9fa3bfca885db8b
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2022-01-02 14:10:56 +0200

    Create Lawyer Input Model

---
+++ LawyerServices.Shared/AdministrationInputModels/CreateLawyerModel.cs
@@ -0,0 +1,36 @@
+﻿using LawyerServices.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LawyerServices.Shared.AdministrationInputModels
+{
+    public class CreateLawyerModel
+    {
+
+        [Required]
+        [StringLength(20, ErrorMessage = "")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "")]
+        public string LastName { get; set; }
+        
+        [Required]
+        public Town TownName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "")]
+        public string AddressLocation { get; set; }
+
+        public string OfficeName { get; set; }
+
+        [Url]
+        public string WebSite { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
--- Services/LawyerServices.Services.Data/AdminServices/LawyerService.cs
+++ Services/LawyerServices.Services.Data/AdminServices/LawyerService.cs
@@ -1,6 +1,23 @@
-﻿namespace LawyerServices.Services.Data.AdminServices
+﻿using LawyerServices.Data.Models;
+using LawyerServices.Data.Repositories;
+
+namespace LawyerServices.Services.Data.AdminServices
 {
     public class LawyerService : ILawyerService
     {
+        private IDeletableEntityRepository<Company> companyRepository;
+
+        private IDeletableEntityRepository<ApplicationUser> userRepository;
+        public LawyerService(IDeletableEntityRepository<Company> companyRepository, IDeletableEntityRepository<ApplicationUser> userRepository)
+        {
+            this.companyRepository = companyRepository;
+            this.userRepository = userRepository;
+        }
+
+        public string CrreateLawyer()
+        {
+            var existingUser = this.userRepository.All().Any();
+            return null;
+        }
     }
 }
