9f882f0c45827b5697c99455dc8dd407cdafd5e0
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2022-04-11 10:57:12 +0300

    Create law firm service and implement createlawfirm function

---
+++ Services/LawyerServices.Services.Data/LawFirmService.cs
@@ -0,0 +1,35 @@
+﻿using LaweyrServices.Web.Shared.LawFirmModels;
+using LawyerServices.Data.Models;
+using LawyerServices.Data.Repositories;
+
+namespace LawyerServices.Services.Data
+{
+    public class LawFirmService : ILawFirmService
+    {
+        private readonly IDeletableEntityRepository<LawFirm> lawFirmrepository;
+        private readonly IDeletableEntityRepository<Town> townRepository;
+        public LawFirmService(IDeletableEntityRepository<LawFirm> lawFirmrepository)
+        {
+            this.lawFirmrepository = lawFirmrepository;
+        }
+
+        public async Task CreateLawFirm(LawFirmInputModel model)
+        {
+
+            var town = this.townRepository.All().FirstOrDefault(x => x.Name.ToLower() == model.Town.ToLower());
+            if (town == null) return;
+            var lawFirm = new LawFirm() 
+            {
+                Name = model.Name,
+                Town = town,
+                Address = model.Address,
+                PhoneNumber = model.PhoneNumber,
+                About = model.About,
+
+            };
+
+            await this.lawFirmrepository.AddAsync(lawFirm);
+            this.lawFirmrepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ Web/LaweyrServices.Web/Shared/LawFirmModels/LawFirmInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace LaweyrServices.Web.Shared.LawFirmModels
+{
+    public class LawFirmInputModel
+    {
+        public string? Name { get; set; }
+
+        public string Town { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? About { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+       
+    }
+}
