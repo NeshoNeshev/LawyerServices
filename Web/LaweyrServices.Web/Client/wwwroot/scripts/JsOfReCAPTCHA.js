356817475a90266519b0f52569a9652b0042e4ce
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2022-06-10 09:23:40 +0300

    Improve css

--- Web/LaweyrServices.Web/Client/wwwroot/scripts/JsOfReCAPTCHA.js
+++ Web/LaweyrServices.Web/Client/wwwroot/scripts/JsOfReCAPTCHA.js
@@ -1,11 +1,18 @@
-﻿function render_recaptcha(dotNetObj, selector, sitekey) {
-    return grecaptcha.render(selector, {
-        'sitekey': sitekey,
-        'callback': (response) => { dotNetObj.invokeMethodAsync('CallbackOnSuccess', response); },
-        'expired-callback': () => { dotNetObj.invokeMethodAsync('CallbackOnExpired'); }
-    });
-};
+﻿////function render_recaptcha(dotNetObj, selector, sitekey) {
+////    return grecaptcha.render(selector, {
+////        'sitekey': sitekey,
+////        'callback': (response) => { dotNetObj.invokeMethodAsync('CallbackOnSuccess', response); },
+////        'expired-callback': () => { dotNetObj.invokeMethodAsync('CallbackOnExpired'); }
+////    });
+////};
 
-function getResponse(widgetId) {
-    return grecaptcha.getResponse(widgetId);
+////function getResponse(widgetId) {
+////    return grecaptcha.getResponse(widgetId);
+////}
+window.getCaptcha = async function () {
+    await grecaptcha.ready(function () { });
+
+    const token = await grecaptcha.execute('XXX', { action: 'formSubmission' })
+
+    return token;
 }
